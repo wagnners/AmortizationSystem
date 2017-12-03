ece3b752296da4457e54f0f78cf519306a2fb5d2
Wagner Luz <wagnnersousa@hotmail.com>
2017-12-03 09:46:35 -0200

    Including the function printPDF and is already printing, but not configured

--- TrabalhoFinalEconomia/src/controller/ControlTelaPrincipal.java
+++ TrabalhoFinalEconomia/src/controller/ControlTelaPrincipal.java
@@ -5,11 +5,24 @@
  */
 package controller;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.TextElementArray;
+import com.itextpdf.text.pdf.PdfWriter;
 import java.awt.Component;
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ComboBoxModel;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JOptionPane;
@@ -27,6 +40,7 @@ public class ControlTelaPrincipal {
 
     Amortizacao a = new Amortizacao();
     CalculoPrincipal p;
+    String stringTableTelaP;
 
     view.TelaPrincipal tela = null;
 
@@ -107,16 +121,23 @@ public class ControlTelaPrincipal {
                 }
             }
         });
-           tela.btLimpar.addActionListener(new ActionListener() {
+        tela.btLimpar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 limpar();
             }
         });
+        
+        tela.btnGerarPDF.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                imprimirPDF();
+            }
+        });
 
     }
-    
-    private void limpar(){
+
+    private void limpar() {
         tela.edDesconto.setText(null);
         tela.edNomeProduto.setText(null);
         tela.edQtdParcelas.setText(null);
@@ -124,27 +145,55 @@ public class ControlTelaPrincipal {
         tela.edValor.setText(null);
     }
 
-    private void calcular() {
-        try{
-        a.setNomeProduto(tela.edNomeProduto.getText());
-        a.setVp(Double.parseDouble(tela.edValor.getText()));
-        a.setDesconto(Double.parseDouble(tela.edDesconto.getText()));
-        a.setTaxa(Double.parseDouble(tela.edTaxa.getText()));
-        a.setParcelas(Integer.parseInt(tela.edQtdParcelas.getText()));       
-        EnumPeriodoTaxa enumPTaxa = (EnumPeriodoTaxa) tela.cbTaxa.getSelectedItem();
-        EnumTipoAmortizacao enumTAmortizacao = (EnumTipoAmortizacao) tela.cbAmortizacao.getSelectedItem();
-        EnumTipoSerie enumTSerie = (EnumTipoSerie) tela.cbSerie.getSelectedItem();
-        p = new CalculoPrincipal(a);
-        p.selecionaPeriodoTaxa(enumPTaxa.getValor());
-        p.selecionaTipoAmortizacao(enumTAmortizacao.getValorAmort());
-        p.selecionaTipoSerie(enumTSerie.getValorSerie());
-        p.calculoTaxa();
-        p.calculoValorPrincipal();
-        p.verificaTipoDeSerie();
-        p.calculoAmortizacao();
-        tela.taTabela.setText(p.calculoAmortizacao());
-         }catch(Exception e){
+    private void setDados() {
+        try {
+            a.setNomeProduto(tela.edNomeProduto.getText());
+            a.setVp(Double.parseDouble(tela.edValor.getText()));
+            a.setDesconto(Double.parseDouble(tela.edDesconto.getText()));
+            a.setTaxa(Double.parseDouble(tela.edTaxa.getText()));
+            a.setParcelas(Integer.parseInt(tela.edQtdParcelas.getText()));
+            EnumPeriodoTaxa enumPTaxa = (EnumPeriodoTaxa) tela.cbTaxa.getSelectedItem();
+            EnumTipoAmortizacao enumTAmortizacao = (EnumTipoAmortizacao) tela.cbAmortizacao.getSelectedItem();
+            EnumTipoSerie enumTSerie = (EnumTipoSerie) tela.cbSerie.getSelectedItem();
+            p = new CalculoPrincipal(a);
+            p.selecionaPeriodoTaxa(enumPTaxa.getValor());
+            p.selecionaTipoAmortizacao(enumTAmortizacao.getValorAmort());
+            p.selecionaTipoSerie(enumTSerie.getValorSerie());
+            p.calculoTaxa();
+            p.calculoValorPrincipal();
+            p.verificaTipoDeSerie();
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(tela, "Você precisa informar todos os campos");
+        }
+    }
+
+    public void calcular() {
+        setDados();
+        tela.taTabela.setText(p.calculoAmortizacao());
+    }
+
+    public void imprimirPDF() {
+   
+            setDados();
+            Document document = new Document();
+    try {
+           PdfWriter.getInstance(document, new FileOutputStream("amortizacao.pdf"));
+           
+           document.open();
+           document.add(new Phrase(p.calculoAmortizacao()));
+           
+
+        } catch (DocumentException | FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(tela, "Erro ao gerar o arquivo " + ex);
+        }
+        finally{
+            document.close();
+        }
+
+        try {
+            Desktop.getDesktop().open(new File("amortizacao.pdf"));
+        } catch (IOException ex) {
+           JOptionPane.showMessageDialog(tela, "Não foi possível abrir o arquivo PDF. Erro: " + ex);
         }
     }
 
